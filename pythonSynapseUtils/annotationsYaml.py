680670dc0fd9c3d7643020f1cc2ab2894842a63e
Kristen Dang <kristen.dang@gmail.com>
2016-08-19 10:56:07 -0700

    Uses walk function, return sets.

--- pythonSynapseUtils/annotationsYaml.py
+++ pythonSynapseUtils/annotationsYaml.py
@@ -1,42 +1,60 @@
 import yaml
+import synapseutils as synu
 
 
-def checkAgainstDict(annotDictSynID, project, syn):
-	''' Compares annotations in use against dictionary.
-	
-	 Gets all annotation keys and values in use in a project and compares against those specified by a dictionary. Prints non-matching terms.
-	'''
-	yamlEnt = syn.get(annotDictSynID)
+
+def checkAgainstDict(syn, synId, annotDictId,verbose=True):	
+	"""
+	Compares annotations in use against dictionary.
+	Gets all annotation keys and values in use in a project and compares against those specified by a dictionary. Prints non-matching terms.
+
+	:param syn:            A Synapse object: syn = synapseclient.login()- Must be logged into synapse
+	:param synId:          A Synapse ID of Project or Folder
+	:param annotDictId     A Synapse ID of annotation dictionary in YAML
+
+	Return:
+		If synId is an ID of a Project/Folder
+			wrongKeys:		    A set of keys in use in synId that are not in annotDictId.
+			wrongValues: 	    A set of values in use in synId that are not in annotDictId.
+
+	Example:
+		wrongKeys, wrongValues = checkAgainstDict(syn,"syn12345","syn45678")
+
+	"""
+
+	yamlEnt = syn.get(annotDictId)
 	with open(yamlEnt.path) as f:
-	    annotations = yaml.load(f)
+		annotations = yaml.load(f)
 
 	allKeysInProject = set()
 	allValsInProject = set()
 
 	systemKeysToExclude = ['creationDate', 'etag', 'id', 'uri', 'accessControl']
 
-	results = syn.chunkedQuery('select id from file where projectId=="%s"' % project)
-	for result in results:
-		temp = syn.getAnnotations(result['file.id'])
-		for key in temp:
-			if key in systemKeysToExclude: continue
-			allKeysInProject.add(key)
-			if isinstance(temp[key], list):
-				for val in temp[key]:
-					allValsInProject.add(str(val))
-			else:
-				allValsInProject.add(str(temp[key]))
+	directory = synu.walk(syn,synId)
+	for dirpath,dirname,files in directory:
+		for item in files:
+			temp = syn.getAnnotations(item[1])
+			for key in temp:
+				if key in systemKeysToExclude: continue
+				allKeysInProject.add(key)
+				if isinstance(temp[key], list):
+					for val in temp[key]:
+						allValsInProject.add(str(val))
+				else:
+					allValsInProject.add(str(temp[key]))
 
 	print 'Number of key terms in project: %d' % len(allKeysInProject)
 	print 'Number of value terms in project: %d' % len(allValsInProject)
-
+	wrongKeys = set()
+	wrongValues = set()
 
 	allKeysInVocab = set(annotations.keys())
-	if not allKeysInProject <= allKeysInVocab:
-		print 'Keys in use that are not found in dictionary: '
-		for item in allKeysInProject.difference(allKeysInVocab):
+	if verbose and not allKeysInProject <= allKeysInVocab:
+		print '\nKeys in use that are not found in dictionary: \n'
+		wrongKeys = allKeysInProject.difference(allKeysInVocab)
+		for item in wrongKeys:
 			print '%s' % item
-
 
 	allValsInVocab = set()
 	for key, vl in annotations.iteritems():
@@ -45,10 +63,13 @@ def checkAgainstDict(annotDictSynID, project, syn):
 				allValsInVocab.add(str(element))
 		else:	
 			allValsInVocab.add(str(vl))
-	if not allValsInProject <= allValsInVocab:
-		print 'Values in use that are not found in dictionary: '
-		for item in allValsInProject.difference(allValsInVocab):
+	if verbose and not allValsInProject <= allValsInVocab:
+		print '\nValues in use that are not found in dictionary: \n'
+		wrongValues = allValsInProject.difference(allValsInVocab)
+		for item in wrongValues:
 			print '%s' % item
+	
+	return(wrongKeys, wrongValues)
 
 
 
@@ -111,10 +132,20 @@ def updateKey(oldKey,newKey,inAnnot):
 
 
 
-def correctAnnot(correctionsFile, project,syn):
-	'''Propagates annotation changes based on tab-delimited input.
-	
+def correctAnnot(syn,synId,projSynId,correctionsFile):
+	"""
+	Propagates annotation changes based on tab-delimited input.
 	Given a tab-separated file containing annotations to be updated, changes annotations across a project. File contains one line per key-value pair, if line has two entries, they are assumed to be oldKey and newKey, if three entries, they are assumed to be key, oldValue, newValue.'''
+
+	:param syn:            A Synapse object: syn = synapseclient.login()- Must be logged into synapse
+	:param synId:          A Synapse ID of Project or Folder
+	:param projSynId:      A Synapse ID of Project (possibly duplicate of synId).
+	:param correctionsFile Path to a tab-delimited file of old and new annotation values.
+
+	Example:
+		correctAnnot(syn,"syn12345","syn45678","annotation_corrections.txt")
+
+	"""
 
 	with open(correctionsFile) as toChange:
 		for line in toChange:
@@ -122,17 +153,17 @@ def correctAnnot(correctionsFile, project,syn):
 			if len(items) == 2: # update keys
 				old = items[0]
 				new = items[1]
-				sql = 'select id,%s from file where projectId=="%s"' % (old,project)
-				results = syn.chunkedQuery(sql)
-				for result in results:
-					temp = syn.getAnnotations(result['file.id'])
+				directory = synu.walk(syn,synId)
+				for dirpath,dirname,files in directory:
+					for item in files:
+					temp = syn.getAnnotations(item[1])
 					if old not in temp: continue
 					correctedAnnotations = updateKey(oldKey=old,newKey=new,inAnnot=temp)
 					savedAnnotations = syn.setAnnotations(result['file.id'],correctedAnnotations)
 			elif len(items) > 2: # update values
 				kKey = items.pop(0)
 				old = items.pop(0)
-				sql = 'select id,%s from file where projectId=="%s" and file.%s=="%s"' % (kKey,project,kKey,old)
+				sql = 'select id,%s from file where projectId=="%s" and file.%s=="%s"' % (kKey,projSynId,kKey,old)
 				results = syn.chunkedQuery(sql)
 				for result in results:
 					temp = syn.getAnnotations(result['file.id'])
