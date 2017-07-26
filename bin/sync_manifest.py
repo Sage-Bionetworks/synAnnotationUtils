e82d54adfb2fed61ba312019218817edeb316063
xindiguo <xindi.guo@sagebase.org>
2017-07-26 13:54:16 -0700

    make the sync_manifest to create manifest only

--- bin/sync_manifest.py
+++ bin/sync_manifest.py
@@ -1,50 +1,8 @@
-'''
-**Create sync manifest**
+#!/usr/bin/env python
 
-1. You can create a sync manifest by simply running 
+'''Create Synapse sync manifest
    
-   $ python sync_manifest.py -c -d home_dir_of_files/ -id syn1234567
-
-    syn1234567 is the Synapse ID of a Synapse Project or a Synapse Folder that is the equivalent to home_dir_of_files in the directory hierarchy. 
-
-    This will generate a tsv file lists out all the files under home_dir_of_files and create mirrored folder hierarchy on Synapse server at the same time. Please note no files has been uploaded at this time. 
-
-2. Add annotation keys
-   If you would like to add a set of annotation keys to the files, you can run
-   
-   $ python sync_manifest.py -c -d home_dir_of_files/ -id syn1234567 -f project_annotations.json
-
-     We suggest you use the set of Sage standard annotation keys from our repo - https://raw.githubusercontent.com/Sage-Bionetworks/synapseAnnotations/master/synapseAnnotations/data/common/minimal_Sage_standard.json
-
-   In this case, the script is 
-
-   $ python sync_manifest.py -c -d home_dir_of_files/ -id syn1234567 -f https://raw.githubusercontent.com/Sage-Bionetworks/synapseAnnotations/master/synapseAnnotations/data/common/minimal_Sage_standard.json
-
-3. Flatten folder hierarchy
-   If you know your folder structure very well and would like to flatten the hierarchy, you can add another parameter '-n'.
-
-   $ python sync_manifest.py -c -d home_dir_of_files/ -id syn1234567 -f https://raw.githubusercontent.com/Sage-Bionetworks/synapseAnnotations/master/synapseAnnotations/data/common/minimal_Sage_standard.json -n 2
-   
-   Once you set n=2, the folder with a depth greater than 2 will be flattened. 
-   For example, if your folder structure on your local server is like below.
-   
-   --- home_dir_of_files
-          --- rnaSeq
-               --- sample1
-                    --- file1.txt
-                    --- file2.csv
-               --- sample2 
-          --- snpArray
-               --- sample1
-               --- sample2
-
-  Since folder 'sample1' under 'rnaSeq' has a depth of 3, the files inside the folder will be renamed to "sample1_file1.txt" and "sample1_file2.csv" and to be placed under "rnaSeq" folder on the Synapse server.
-
-**Upload and annotate files via sync function**
-
-Once you made all the edits to your sync manifest (default manifest name is "output.tsv") to Synapse.
-
-$ python sync_manifest.py -u -m output.tsv
+   In tab delimited format
 
 '''
 
@@ -147,55 +105,39 @@ def create(file_list,key_list,synapse_dir,local_root,depth):
     result = pd.concat([result,pd.DataFrame(columns=key_list)])
     result = result[cols + key_list] # reorder the columns
     
-    stdout = sys.stdout # keep a local copy
-    
-    sys.stdout = open("output.tsv",'w')
     result.to_csv(sys.stdout, sep="\t",index=False)
-    sys.stdout.close()
     
-    sys.stdout = stdout # put it back
-    
-    sys.stderr.write('Manifest has been created. Stored as \"output.tsv\"\n')
+    sys.stderr.write('Manifest has been created.')
 
 def main():
     import argparse
-    syn = synapseclient.login()
+    syn = synapseclient.login(silent=True)
     
-    parser = argparse.ArgumentParser(description="Create or upload sync manifest")
-    parser.add_argument('-c','--create', help='create manifest', action='store_true')
-    parser.add_argument('-u','--upload', help='sync to synapse using manifest', action='store_true')
+    parser = argparse.ArgumentParser(description="Create Synapse sync manifest")
     parser.add_argument('-d','--directory', help='local directory')
-    parser.add_argument('--id',help='Synapse ID of the project/file')
+    parser.add_argument('--id',help='Synapse ID of the project/folder')
     parser.add_argument('-f','--files',
                         help='Path(s) to JSON file(s) of annotations. optional', nargs='+')
     parser.add_argument('-n','--n', help='depth of hierarchy, DEFAULT is None', default=None)
-    parser.add_argument('-m','--manifest',help='manifest file to be uploaded')
     
     args=parser.parse_args()
 
+    sys.stderr.write('Preparing to create manifest\n')
+    local_root = args.directory
+    synapse_id = args.id
+    annotations = args.files
+    depth = args.n
+
+    if depth is not None:
+        depth = int(depth) 
+
+    dir_list, file_list = _getLists(local_root,depth)
+
+    synapse_dir = _getSynapseDir(syn, synapse_id,local_root,dir_list)
+    key_list = _getAnnotationKey(annotations)
+
+    create(file_list,key_list,synapse_dir,local_root,depth)
     
-    if args.create:
-        sys.stderr.write('Preparing to create manifest\n')
-        local_root = args.directory
-        synapse_id = args.id
-        annotations = args.files
-        depth = args.n
-
-        if depth is not None:
-            depth = int(depth) 
-
-        dir_list, file_list = _getLists(local_root,depth)
-
-        synapse_dir = _getSynapseDir(syn, synapse_id,local_root,dir_list)
-        key_list = _getAnnotationKey(annotations)
-
-        create(file_list,key_list,synapse_dir,local_root,depth)
-    elif args.upload:
-        sys.stderr.write('Preparing to upload files\n')
-        synu.syncToSynapse(syn, args.manifest)
-    else:
-        sys.stderr.write('Please enter python sync_manifest.py -h for more information.\n')
-
 if __name__ == '__main__':
     main()
 
