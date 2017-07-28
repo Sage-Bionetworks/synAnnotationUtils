cb42217b6b5400e0a319d343988d7a9c5b09d592
kdaily <kenneth.daily@sagebase.org>
2017-07-28 10:55:19 -0700

    minor fixes

--- bin/sync_to_synapse.py
+++ bin/sync_to_synapse.py
@@ -1,14 +1,20 @@
-'''Upload files using the Synapse sync manifest
+'''Upload files using the Synapse sync manifest.
 
-    This is a temporary helper script
-    Next Synapse client release will replace this script.
+This is a temporary helper script.
+The Synapse client release 1.7.3 will replace this script.
+
 '''
+
 import argparse
 
 import synapseclient
 import synapseutils
 
 def sync(args, syn):
+    """Run syncToSynapse using command line arguments.
+
+    """
+
     synapseutils.syncToSynapse(syn, manifest_file=args.manifestFile,
                                dry_run=args.dryRun, sendMessages=args.sendMessages,
                                retries=args.retries)
@@ -17,11 +23,12 @@ def main():
     syn = synapseclient.login()
 
     parser_sync = argparse.ArgumentParser(description='Synchronize files described in a manifest to Synapse')
-    parser_sync.add_argument('--dryRun', action='store_true', default=False, help='Perform validation without uploading.')
+    parser_sync.add_argument('--dryRun', action='store_true', default=False,
+                             help='Perform validation without uploading.')
     parser_sync.add_argument('--sendMessages', action='store_true', default=False,
                              help='Send notifications via Synapse messaging (email) at specific intervals, on errors and on completion.')
     parser_sync.add_argument('--retries', metavar='INT', type=int, default=4)
-    parser_sync.add_argument('manifestFile',  metavar='FILE', type=str,
+    parser_sync.add_argument('manifestFile', metavar='FILE', type=str,
                              help='A tsv file with file locations and metadata to be pushed to Synapse.')
 
     args = parser_sync.parse_args()
