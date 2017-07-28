08cc65c8a08f0500167a5bfcd078066bcc077fcd
xindiguo <xindi.guo@sagebase.org>
2017-07-27 22:38:02 -0700

    add temporary helper script before sync is in command line client

---
+++ bin/sync_to_synapse.py
@@ -0,0 +1,31 @@
+'''Upload files using the Synapse sync manifest
+
+    This is a temporary helper script
+    Next Synapse client release will replace this script.
+'''
+import argparse
+
+import synapseclient
+import synapseutils
+
+def sync(args, syn):
+    synapseutils.syncToSynapse(syn, manifest_file=args.manifestFile,
+                               dry_run=args.dryRun, sendMessages=args.sendMessages,
+                               retries=args.retries)
+
+def main():
+    syn = synapseclient.login()
+
+    parser_sync = argparse.ArgumentParser(description='Synchronize files described in a manifest to Synapse')
+    parser_sync.add_argument('--dryRun', action='store_true', default=False, help='Perform validation without uploading.')
+    parser_sync.add_argument('--sendMessages', action='store_true', default=False,
+                             help='Send notifications via Synapse messaging (email) at specific intervals, on errors and on completion.')
+    parser_sync.add_argument('--retries', metavar='INT', type=int, default=4)
+    parser_sync.add_argument('manifestFile',  metavar='FILE', type=str,
+                             help='A tsv file with file locations and metadata to be pushed to Synapse.')
+
+    args = parser_sync.parse_args()
+    sync(args, syn)
+
+if __name__ == "__main__":
+    main()
